ba47a546f3d8bbe75816a6a1f8712e56de4edda9
madhusudhanarao-pulimi <167522624+madhusudhanarao-pulimi@users.noreply.github.com>
2024-09-04 14:49:03 +0530

    Update index.js
    Update index.js

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,20 +1,22 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+//const core = require('@actions/core');
+//const github = require('@actions/github');
 
 
 
 try {
   // Fetch the value of the input 'who-to-greet' specified in action.yml
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  //const nameToGreet = core.getInput('who-to-greet');
+  //console.log(`Hello ${nameToGreet}!`);
+
+  console.log(`Hello world, this is my first action!`);
   
   // Record the time of greeting as an output
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  //const time = (new Date()).toTimeString();
+ // core.setOutput("time", time);
 
 
 
 } catch (error) {
   // Handle errors and indicate failure
-  core.setFailed(error.message);
+  //core.setFailed(error.message);
 }
